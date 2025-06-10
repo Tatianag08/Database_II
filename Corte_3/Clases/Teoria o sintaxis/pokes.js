75951a4a99b4a6d46ad27a8ce032aa4136cad320
Tatianag08 <tatiana.gualteros.ayala@gmail.com>
2025-06-09 20:48:56 -0500

    feat: agregar dataset de Pokémon con atributos
    Incluye un arreglo de objetos que representan Pokémon con propiedades como nombre, poder, tipo elemental, nivel de poder, descripción, altura y peso. 
    Útil para pruebas, visualizaciones o ejercicios de consultas en bases de datos.

---
+++ Corte_3/Clases/Teoria o sintaxis/pokes.js
@@ -0,0 +1,10 @@
+const pokes = [
+  {nombre:"Pikachu",poder:"Impactrueno",elemento:"eléctrico",nivel_poder:85,descripcion:"Un Pokémon eléctrico muy popular, conocido por su ternura y agilidad en combate.",altura_m:0.4,peso_kg:6.0},
+  {nombre:"Charizard",poder:"Llamarada",elemento:"fuego",nivel_poder:95,descripcion:"Dragón alado con un temperamento ardiente. Lanza fuego por su boca.",altura_m:1.7,peso_kg:90.5},
+  {nombre:"Bulbasaur",poder:"Látigo Cepa",elemento:"planta",nivel_poder:70,descripcion:"Tiene una semilla en su espalda que crece a medida que lo hace.",altura_m:0.7,peso_kg:6.9},
+  {nombre:"Squirtle",poder:"Pistola Agua",elemento:"agua",nivel_poder:72,descripcion:"Usa su caparazón como protección y lanza potentes chorros de agua.",altura_m:0.5,peso_kg:9.0},
+  {nombre:"Jigglypuff",poder:"Canto",elemento:"hada",nivel_poder:60,descripcion:"Su canto hace dormir a sus oponentes. Tiene una voz dulce y peligrosa.",altura_m:0.5,peso_kg:5.5},
+  {nombre:"Gengar",poder:"Bola Sombra",elemento:"fantasma",nivel_poder:88,descripcion:"Suele aparecer en la oscuridad. Se alimenta del miedo.",altura_m:1.5,peso_kg:40.5},
+  {nombre:"Snorlax",poder:"Golpe Cuerpo",elemento:"normal",nivel_poder:90,descripcion:"Duerme la mayor parte del tiempo, pero es muy fuerte cuando se activa.",altura_m:2.1,peso_kg:460.0},
+];
+
